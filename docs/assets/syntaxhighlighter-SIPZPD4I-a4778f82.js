ea5201ae3187a014a98e2dae188120dc89390527
Juan Montero <juanca.montero76@gmail.com>
2023-07-19 16:54:27 -0500

    change build script

--- docs/assets/syntaxhighlighter-SIPZPD4I-a4778f82.js
+++
@@ -1,2 +0,0 @@
-import{S as d,c as f,s as g}from"./index-6cf35b36.js";import"./iframe-59c37260.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-379ca6ec.js";import"./assert-a1982797.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{d as SyntaxHighlighter,f as createCopyToClipboardFunction,g as default};
-//# sourceMappingURL=syntaxhighlighter-SIPZPD4I-a4778f82.js.map
